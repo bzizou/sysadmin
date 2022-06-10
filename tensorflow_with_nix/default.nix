9ca289dba64de93e430779bfcf8422ea1ae74c17
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2022-06-10 09:30:51 +0200

    More complete tensorflow env (added opencv4 as an example of supplementary dep)

--- tensorflow_with_nix/default.nix
+++ tensorflow_with_nix/default.nix
@@ -1,16 +1,29 @@
 with import <nixpkgs> {};
-mkShell {
+mkShell rec {
   name = "tensorflow-cuda-shell";
+  # libPath and xlibPath provided here with probably more things than you need
+  # if you remove the example opencv4 buildInput given here as an example of supplementary
+  # dependency. Feel free to cistomize.
+  xlibPath = with pkgs.xlibs ; lib.makeLibraryPath [ libX11 libXdmcp libXau libXext libxcb ]; 
+  libPath = with pkgs ; lib.makeLibraryPath [ 
+                          stdenv.cc.cc cudatoolkit_11 cudnn_cudatoolkit_11 
+                          libtiff openssl zlib libbsd expat glib libjpeg libpng
+                          libffi libvorbis gnutls libdrm gmp numactl elfutils
+	                  libelf libogg p11-kit libtasn1 nettle
+                        ]; 
+  # Example including opencv4 as a 
   buildInputs = with python3.pkgs; [ 
     pip
     numpy
     setuptools
+    opencv4 
   ] ++ [ lsb-release ];
   shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudatoolkit_11}/lib:${pkgs.cudnn_cudatoolkit_11}/lib:${pkgs.cudatoolkit_11.lib}/lib:/usr/lib/x86_64-linux-gnu:${pkgs.expat}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH:
+    export LD_LIBRARY_PATH=${libPath}:${xlibPath}:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
     alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' TMPDIR='$HOME' \pip"
     export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.9/site-packages:$PYTHONPATH"
     export PATH="$(pwd)/_build/pip_packages/bin:$PATH"
     unset SOURCE_DATE_EPOCH
   '';
 }
+
