df2049c8bae9bca1605704db1fe417e6a09e6fbc
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2024-02-14 16:41:28 +0100

    Cuda pytorch flake

---
+++ nix_environments/cuda_pytorch-flake/README.md
@@ -0,0 +1,1 @@
+Contribution of SOFYA DYMCHENKO <sofya.dymchenko@univ-grenoble-alpes.fr> which made this flake based on ../cuda_pytorch
---
+++ nix_environments/cuda_pytorch-flake/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Flake: Python 3.10.11 environment with Pytorch 2.0.0 supporting CUDA 11.8 on bigfoot";
+
+  inputs = {
+    # using version of nixpkgs where pytorch version is 2.0
+    nixpkgs.url = "github:nixos/nixpkgs/904f1e3235d78269b5365f2166179596cbdedd66";
+  };
+
+  nixConfig.bash-prompt = "\\e[35m\[nix-develop (\\h)\]\\e[34m\\w\\e[39m$ ";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        allowUnfree = true;
+        cudaSupport = true;
+    };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShells.${system} = rec {
+        sshell = pkgs.mkShell {
+          xlibPath = with pkgs.xorg; pkgs.lib.makeLibraryPath [ libX11 libXdmcp libXau libXext libxcb ];
+          libPath = with pkgs ; pkgs.lib.makeLibraryPath [ 
+                          stdenv.cc.cc cudaPackages_11.cudatoolkit
+                          libtiff openssl zlib libbsd expat glib libjpeg libpng
+                          libffi libvorbis gnutls libdrm gmp numactl elfutils
+	                  libelf libogg p11-kit libtasn1 nettle e2fsprogs libgcrypt
+                          keyutils bzip2 libgpg-error xz libllvm
+                        ];
+          buildInputs = [
+           ( pkgs.python3.withPackages(ps: with ps; [
+                matplotlib
+                numpy
+                pandas
+                scikit-learn
+                scipy
+                pytorch-bin
+          ]))
+         ] ++ [pkgs.lsb-release];
+         shellHook= ''
+           export LD_LIBRARY_PATH=${sshell.libPath}:${sshell.xlibPath}:${pkgs.cudaPackages_11.cudatoolkit}/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+         '';  
+        };
+      };
+    };
+}
