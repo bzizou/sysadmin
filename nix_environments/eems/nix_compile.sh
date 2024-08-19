b44269014808827018b89dbf3029d72400b8a0c8
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2024-08-19 15:20:52 +0200

    Removed useless extra space character

--- nix_environments/eems/nix_compile.sh
+++ nix_environments/eems/nix_compile.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env nix-shell
 #!nix-shell -i bash -p boost -p gcc -p gnumake -p eigen
-#! nix-shell -I nixpkgs=channel:nixos-22.05
+#!nix-shell -I nixpkgs=channel:nixos-22.05
 
 export BOOST_INC=`echo $NIX_CFLAGS_COMPILE| grep -o -P '/nix/store[^ ]+boost[^ ]+'|head -1`
 export BOOST_LIB=`echo $NIX_LDFLAGS |grep -o -P '/nix/store[^ ]+boost[^ ]+/lib'|head -1`
