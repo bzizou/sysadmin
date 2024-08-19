a30723867d4f877aa7893718a031c5437fae9c90
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2024-08-19 15:12:53 +0200

    Added a compilation script using nix-shell for EEMS

---
+++ nix_environments/eems/README.md
@@ -0,0 +1,5 @@
+
+https://github.com/dipetkov/eems
+
+nix_compile.sh: 
+Compilation script to place into `eems/runeems_snps/src` before running from this directory
---
+++ nix_environments/eems/nix_compile.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env nix-shell
+#!nix-shell -i bash -p boost -p gcc -p gnumake -p eigen
+#! nix-shell -I nixpkgs=channel:nixos-22.05
+
+export BOOST_INC=`echo $NIX_CFLAGS_COMPILE| grep -o -P '/nix/store[^ ]+boost[^ ]+'|head -1`
+export BOOST_LIB=`echo $NIX_LDFLAGS |grep -o -P '/nix/store[^ ]+boost[^ ]+/lib'|head -1`
+export EIGEN_INC=`echo $NIX_CFLAGS_COMPILE| grep -o -P '/nix/store[^ ]+eigen[^ ]+'|head -1`
+
+make EIGEN_INC="$EIGEN_INC/eigen3" BOOST_LIB=$BOOST_LIB BOOST_INC=$BOOST_INC linux
