a42011c4770556348f4ed01d3b29530448fcee6e
Bruno Bzeznik <Bruno.Bzeznik@univ-grenoble-alpes.fr>
2025-02-07 13:55:29 +0100

    Cuda-toolkit nix-shell snippet

---
+++ nix_environments/cuda-toolkit/default.nix
@@ -0,0 +1,20 @@
+
+#Snippet for cuda-toolkit, thanks to <Marc.Coiffier@univ-grenoble-alpes.fr> 
+# as nixpkgs gcc is too recent to work with nvcc (here, we are using
+# gcc11 in place of the default gcc12 of stdenv)
+
+let nixpkgs = import ./env-bigfoot-nixpkgs.nix;
+
+in nixpkgs.mkShell.override { stdenv = nixpkgs.stdenvNoCC; } {
+  buildInputs = with nixpkgs; [
+    gcc11
+    cmake
+    pkg-config
+    (nur.repos.gricad.openmpi4.override {
+      cudaSupport = true;
+      inherit cudatoolkit;
+    })
+    cudatoolkit
+  ];
+  NIX_SHELL_PROMPT_TAG = "cudatoolkit";
+}
