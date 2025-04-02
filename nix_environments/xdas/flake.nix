37202bffbe117c77bf4190c5e3defdd6457bb7f2
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2025-04-02 10:49:44 +0200

    Flake can even be installed directly from github

--- nix_environments/xdas/flake.nix
+++ nix_environments/xdas/flake.nix
@@ -4,6 +4,8 @@
 #   nix run
 # It can also be installed into the profile:
 #   nix profile install
+# or (directly from github!):
+#  nix profile install github:bzizou/sysadmin?dir=nix_environments/xdas
 #
 # Customize your python packages in the lines with the "#### HERE" comment
 # Also customize your pinned nixpkgs version in the inputs
