893d7d2ea9e6a6c8bd8e099d2709170d99b1ac68
Bruno Bzeznik <Bruno.Bzeznik@univ-grenoble-alpes.fr>
2025-04-02 11:38:03 +0200

    Added flake for R with packages

---
+++ nix_environments/R/flake.nix
@@ -0,0 +1,85 @@
+# A flake for python that can be called either as a dev shell:
+#   nix develop
+# or that can be ran as a python interpreter:
+#   nix run
+# It can also be installed into the profile:
+#   nix profile install
+#
+# Customize your R packages in the lines with the "#### HERE" comment
+# Also customize your pinned nixpkgs version in the inputs
+
+{
+  description = "Flake: R with Julia";
+
+  inputs = {
+    nixpkgs.url = "github:bzizou/nixpkgs/a33463999d9599a0c339f9df7a7ff31c39d1b799";
+  };
+
+  nixConfig.bash-prompt = "\\e[35m\[nix-develop (\\h)\]\\e[34m\\w\\e[39m$ ";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        allowUnfree = true;
+      };
+      shell = pkgs.mkShell rec {
+          libPath = with pkgs ; pkgs.lib.makeLibraryPath [ 
+                          stdenv.cc.cc
+                          openssl zlib 
+                        ];
+          myR = pkgs.rWrapper.override{ packages = with pkgs.rPackages; [
+
+#### HERE goes the R packages list #### 
+                 devtools
+                 JuliaCall
+                 knitr
+                 remotes
+                 raster
+                 ggplot2
+                 plyr
+                 dplyr
+                 magrittr
+                 lme4
+                 spatstat_explore
+                 spatstat_geom
+                 spdep
+                 Matrix
+                 MuMIn
+                 doParallel
+                 RcppArmadillo
+                 rjson
+                 foreach
+                 rmarkdown
+                 ggExtra
+                 igraph
+                 terra
+                 sp
+############################################
+
+          ]; };
+          buildInputs = [ ( myR ) ] ++ [pkgs.lsb-release];
+          shellHook= ''
+             export LD_LIBRARY_PATH=${shell.libPath}:$LD_LIBRARY_PATH
+          '';  
+      };
+      runner = pkgs.writeShellApplication {
+        name = "my-R";
+        runtimeInputs = shell.buildInputs;
+        text = ''
+          ${shell.myR}/bin/R
+        '';
+      };
+    in
+    {
+      packages.x86_64-linux.default = shell.myR;
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      devShells.${system}.default = shell; 
+      apps.${system}.default = {
+        type = "app";
+        pname = "my-R";
+        program = "${runner}/bin/my-R";
+      };
+    };
+}
