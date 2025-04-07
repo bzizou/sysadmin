32dd171161f9dc4421b27770aab65a4c27f06710
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2025-04-07 10:09:49 +0200

    New python flake example

--- nix_environments/python3_pinned-flake/flake.nix
+++ nix_environments/python3_pinned-flake/flake.nix
@@ -1,8 +1,20 @@
+# A flake for python that can be called either as a dev shell:
+#   nix develop
+# or that can be ran as a python interpreter:
+#   nix run
+# It can also be installed into the profile:
+#   nix profile install
+# or (directly from github!):
+#  nix profile install github:bzizou/sysadmin?dir=nix_environments/xdas
+#
+# Customize your python packages in the lines with the "#### HERE" comment
+# Also customize your pinned nixpkgs version in the inputs
+
 {
   description = "Flake: Python 3.12.17";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/2941267bd2be3070b98b779910fdf12eb81979e6";
+    nixpkgs.url = "github:bzizou/nixpkgs/a33463999d9599a0c339f9df7a7ff31c39d1b799";
   };
 
   nixConfig.bash-prompt = "\\e[35m\[nix-develop (\\h)\]\\e[34m\\w\\e[39m$ ";
@@ -13,27 +25,71 @@
       pkgs = import nixpkgs {
         inherit system;
         allowUnfree = true;
-    };
-    in
-    {
-      formatter.${system} = pkgs.nixpkgs-fmt;
-
-      devShells.${system} = rec {
-        default = pkgs.mkShell {
-          xlibPath = with pkgs.xorg; pkgs.lib.makeLibraryPath [ libX11 libXdmcp libXau libXext libxcb ];
+      };
+      shell = pkgs.mkShell rec {
           libPath = with pkgs ; pkgs.lib.makeLibraryPath [ 
                           stdenv.cc.cc
                           openssl zlib 
                         ];
-          buildInputs = [
-           ( pkgs.python3.withPackages(ps: with ps; [
-                numpy
-          ]))
-         ] ++ [pkgs.lsb-release];
-         shellHook= ''
-             export LD_LIBRARY_PATH=${default.libPath}:$LD_LIBRARY_PATH
-         '';  
-        };
+
+#### HERE goes the custom python packages definitions ################ 
+          mypip = pkgs.python3.pkgs.buildPythonPackage rec {
+                   name = "pip";
+                   src = pkgs.fetchFromGitHub {
+                     owner = "pypa";
+                     repo = "pip";
+                     rev = "22.3";
+                     sha256 = "sha256-4PhpaycF69mR0xMI7Z5qJRnaql+g8suEfrkUn8AfM50=";
+                   };
+                   doCheck = false;
+                   pipInstallFlags = [ "--ignore-installed" ];
+          };
+
+         myprecommit= pkgs.python3.pkgs.buildPythonPackage rec {
+                   name = "precommit";
+                   src = pkgs.fetchFromGitHub {
+                     owner = "pre-commit";
+                     repo = "pre-commit";
+                     rev = "v4.2.0";
+                     sha256 = "sha256-rUhI9NaxyRfLu/mfLwd5B0ybSnlAQV2Urx6+fef0sGM=";
+                   };
+          };
+
+#######################################################################
+ 
+          mypy = pkgs.python3.withPackages(ps: with ps; [
+
+#### HERE goes the Python packages list #### 
+                 cfgv
+                 identify
+                 pyyaml
+                 nodeenv
+                 mypip
+                 myprecommit
+###########################################
+
+          ]);
+          buildInputs = [ ( mypy ) ] ++ [pkgs.lsb-release];
+          shellHook= ''
+             export LD_LIBRARY_PATH=${shell.libPath}:$LD_LIBRARY_PATH
+          '';  
+      };
+      runner = pkgs.writeShellApplication {
+        name = "my-python";
+        runtimeInputs = shell.buildInputs;
+        text = ''
+          ${shell.mypy.interpreter}
+        '';
+      };
+    in
+    {
+      packages.x86_64-linux.default = shell.mypy;
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      devShells.${system}.default = shell; 
+      apps.${system}.default = {
+        type = "app";
+        pname = "my-python";
+        program = "${runner}/bin/my-python";
       };
     };
 }
